169c1efd7eac8b5f821d5e68dd09f10da2ba976e
HP <rrborade4@gmail.com>
2026-02-20 11:24:43 +0530

    feat(form): add folder Rohit,  Component - AdminForm30PlusInput, Form with Yup validation

--- LoginForm/src/main.jsx
+++ LoginForm/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import LoginForm from './Components/LoginForm.jsx'
+import LoginForm from './Rohit/Components/LoginForm.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginForm />
+    <App />
   </StrictMode>,
 )
